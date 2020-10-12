07d138f8f4c6912368aa6b0ac57be2c8f23d211f
Lester <marclesterlagunday@gmail.com>
2020-10-12 13:24:43 +0800

    fix js

--- script.js
+++ script.js
@@ -18,30 +18,30 @@ for(var i=0; themeDots.length > i; i++){
 function setTheme(mode){
     if(mode == 'default'){
         document.getElementById('theme-style').href = 'default.css';
-        document.getElementById('url_color_1').content = '#eaeaea';
-        document.getElementById('url_color_2').content = '#eaeaea';
-        document.getElementById('url_color_3').content = '#eaeaea';
+        document.getElementById('url-color-1').content = '#eaeaea';
+        document.getElementById('url-color-2').content = '#eaeaea';
+        document.getElementById('url-color-3').content = '#eaeaea';
     }
 
     if(mode == 'blue'){
         document.getElementById('theme-style').href = 'blue.css';
-        document.getElementById('url_color_1').content = '#15202b';
-        document.getElementById('url_color_2').content = '#15202b';
-        document.getElementById('url_color_3').content = '#15202b';
+        document.getElementById('url-color-1').content = '#15202b';
+        document.getElementById('url-color-2').content = '#15202b';
+        document.getElementById('url-color-3').content = '#15202b';
     }
 
     if(mode == 'green'){
         document.getElementById('theme-style').href = 'green.css';
-        document.getElementById('url_color_1').content = '#606b56';
-        document.getElementById('url_color_2').content = '#606b56';
-        document.getElementById('url_color_3').content = '#606b56';
+        document.getElementById('url-color-1').content = '#606b56';
+        document.getElementById('url-color-2').content = '#606b56';
+        document.getElementById('url-color-3').content = '#606b56';
     }
 
     if(mode == 'purple'){
         document.getElementById('theme-style').href = 'purple.css';
-        document.getElementById('url_color_1').content = '#46344e';
-        document.getElementById('url_color_2').content = '#46344e';
-        document.getElementById('url_color_3').content = '#46344e';
+        document.getElementById('url-color-1').content = '#46344e';
+        document.getElementById('url-color-2').content = '#46344e';
+        document.getElementById('url-color-3').content = '#46344e';
     }
 
     localStorage.setItem('theme', mode);
